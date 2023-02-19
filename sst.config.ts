96c62c6119bee8312e4c67e76b5de40d6a0f6b49
Raibima Putra <rayp1100@gmail.com>
2023-02-19 07:38:53 +0700

    Init SST

---
+++ sst.config.ts
@@ -0,0 +1,21 @@
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "dropin-next",
+      region: "ap-southeast-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site(ctx) {
+      const site = new NextjsSite(ctx.stack, "dropin-next-app", {
+        path: ".",
+      });
+      ctx.stack.addOutputs({
+        SiteUrl: site.url || "http://localhost:3000",
+      });
+    });
+  },
+} satisfies SSTConfig;
