98610a2cf7f79bae90f8b466f55d664758d7c9b6
Raibima Putra <rayp1100@gmail.com>
2023-02-19 07:45:07 +0700

    GH action part 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+};
+
+if (!process.env.FOO) {
+  throw new Error("process.env.FOO is not defined");
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
